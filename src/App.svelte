0d8df24016b8d4d7eab9789fac4fe1ab54531914
Lucidreline <casta.ma502@gmail.com>
2021-04-16 19:04:26 -0700

    Init project and centered a list.

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<div id="body">
+  <div id="to-do-list">
+    <div id="list-header">
+      <ul id="list-header-content">
+        <li>-</li>
+        <li>Tasks</li>
+        <li>+</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  #body {
+    display: grid;
+    place-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: red;
+  }
+
+  #list-header {
+    color: white;
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
