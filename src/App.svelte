3c23e6ec0425196c8885fe1b28df68c4caab2c01
Lucidreline <casta.ma502@gmail.com>
2021-04-17 15:48:44 -0700

    Added a list header.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <div id="body">
   <div id="to-do-list">
     <div id="list-header">
-      <ul id="list-header-content">
-        <li>-</li>
-        <li>Tasks</li>
-        <li>+</li>
-      </ul>
+      <div id="list-header-content">
+        <span>-</span>
+        <span>Tasks</span>
+        <span>+</span>
+      </div>
     </div>
   </div>
 </div>
@@ -24,9 +24,24 @@
     background-color: red;
   }
 
+  #to-do-list {
+    width: 15rem;
+  }
+
   #list-header {
+    background-color: orange;
+  }
+
+  #list-header-content {
+    width: 100%;
     color: white;
     display: flex;
     justify-content: space-around;
+    list-style: none;
+    text-align: center;
+    padding: 0.5rem 0;
+  }
+
+  #list-header-content span {
   }
 </style>
